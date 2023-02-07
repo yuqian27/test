7dbaad8f645048687995bb25131cd1c70ac8476a
yuqian27 <78253776+yuqian27@users.noreply.github.com>
2023-02-07 14:51:01 +0800

    Delete assets/js directory

--- assets/js/about-legacy.3a7cca10.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkgamea"]=self["webpackChunkgamea"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=n(1001),l={},r=(0,s.Z)(l,a,u,!1,null,null,null),c=r.exports}}]);
-//# sourceMappingURL=about-legacy.3a7cca10.js.map
